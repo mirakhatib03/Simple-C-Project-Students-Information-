6aecc0415c3309ca643be1fb0935b216fcedf2b1
mirakhatib03 <meeraismael.k@gmail.com>
2025-08-03 21:29:02 +0300

    Add files via upload

---
+++ Assignment 3/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿
+namespace Assignment_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbMonths = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lsbMembership = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbxOptions = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.lsbSelectedOptions = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblTotalFee = new System.Windows.Forms.Label();
+            this.lblMonthlyFee = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txbMonths);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Tai Le", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(72, 291);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(227, 142);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Membership Length";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(14, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(189, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Enter the Number of Months:";
+            // 
+            // txbMonths
+            // 
+            this.txbMonths.Location = new System.Drawing.Point(17, 79);
+            this.txbMonths.Name = "txbMonths";
+            this.txbMonths.Size = new System.Drawing.Size(182, 24);
+            this.txbMonths.TabIndex = 2;
+            this.txbMonths.TextChanged += new System.EventHandler(this.txbMonths_TextChanged);
+            this.txbMonths.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbMonths_KeyPress);
+            // 
+            // lsbMembership
+            // 
+            this.lsbMembership.Font = new System.Drawing.Font("Microsoft Tai Le", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lsbMembership.FormattingEnabled = true;
+            this.lsbMembership.ItemHeight = 16;
+            this.lsbMembership.Items.AddRange(new object[] {
+            "Standard Adult",
+            "Child (12 && Under)",
+            "Student",
+            "Senior Citizen"});
+            this.lsbMembership.Location = new System.Drawing.Point(77, 71);
+            this.lsbMembership.Name = "lsbMembership";
+            this.lsbMembership.Size = new System.Drawing.Size(222, 180);
+            this.lsbMembership.TabIndex = 0;
+            this.lsbMembership.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Type of Membership";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(411, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Options";
+            // 
+            // cbxOptions
+            // 
+            this.cbxOptions.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.cbxOptions.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxOptions.FormattingEnabled = true;
+            this.cbxOptions.Items.AddRange(new object[] {
+            "Yoga ",
+            "Karate ",
+            "Personal Trainer"});
+            this.cbxOptions.Location = new System.Drawing.Point(414, 71);
+            this.cbxOptions.Name = "cbxOptions";
+            this.cbxOptions.Size = new System.Drawing.Size(269, 24);
+            this.cbxOptions.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.SystemColors.Window;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Tai Le", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(736, 72);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(89, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.BackColor = System.Drawing.SystemColors.Window;
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft Tai Le", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.Location = new System.Drawing.Point(736, 184);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(89, 23);
+            this.btnRemove.TabIndex = 5;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = false;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // lsbSelectedOptions
+            // 
+            this.lsbSelectedOptions.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.lsbSelectedOptions.FormattingEnabled = true;
+            this.lsbSelectedOptions.ItemHeight = 16;
+            this.lsbSelectedOptions.Location = new System.Drawing.Point(414, 135);
+            this.lsbSelectedOptions.Name = "lsbSelectedOptions";
+            this.lsbSelectedOptions.Size = new System.Drawing.Size(269, 116);
+            this.lsbSelectedOptions.TabIndex = 6;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.groupBox2.Controls.Add(this.lblTotalFee);
+            this.groupBox2.Controls.Add(this.lblMonthlyFee);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Tai Le", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(414, 292);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(227, 141);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Membership Fees";
+            // 
+            // lblTotalFee
+            // 
+            this.lblTotalFee.AutoSize = true;
+            this.lblTotalFee.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblTotalFee.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTotalFee.Location = new System.Drawing.Point(102, 94);
+            this.lblTotalFee.Name = "lblTotalFee";
+            this.lblTotalFee.Size = new System.Drawing.Size(114, 20);
+            this.lblTotalFee.TabIndex = 11;
+            this.lblTotalFee.Text = "                          ";
+            // 
+            // lblMonthlyFee
+            // 
+            this.lblMonthlyFee.AutoSize = true;
+            this.lblMonthlyFee.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblMonthlyFee.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMonthlyFee.Location = new System.Drawing.Point(102, 59);
+            this.lblMonthlyFee.Name = "lblMonthlyFee";
+            this.lblMonthlyFee.Size = new System.Drawing.Size(114, 20);
+            this.lblMonthlyFee.TabIndex = 10;
+            this.lblMonthlyFee.Text = "                          ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(53, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Total:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(8, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Monthly Fee:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.Window;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Tai Le", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(332, 477);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(89, 23);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.Window;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Tai Le", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(453, 477);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(89, 23);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.SystemColors.Window;
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Tai Le", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(210, 477);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(89, 23);
+            this.btnCalculate.TabIndex = 10;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(851, 521);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.lsbSelectedOptions);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cbxOptions);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lsbMembership);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Membership Fee calculator";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ListBox lsbMembership;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbMonths;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbxOptions;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.ListBox lsbSelectedOptions;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblTotalFee;
+        private System.Windows.Forms.Label lblMonthlyFee;
+    }
+}
+
---
+++ Assignment 3/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (cbxOptions.SelectedIndex > -1)
+            {
+                object item1 = cbxOptions.SelectedItem;
+                lsbSelectedOptions.Items.Add(item1);
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lsbSelectedOptions.SelectedIndex > -1)
+            {
+                lsbSelectedOptions.Items.RemoveAt(lsbSelectedOptions.SelectedIndex);
+            }
+        }
+        int month;
+        double mbf;
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double total = 0;
+            string m = txbMonths.Text;
+            if (int.TryParse(m, out month))
+            {
+                if (month < 1 || month > 24)
+                {
+                    MessageBox.Show("Wrong value", "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    if (lsbMembership.SelectedIndex==0)
+                    {
+                        mbf = 40;
+                    }
+                    else if (lsbMembership.SelectedIndex == 1)
+                    {
+                        mbf = 20;
+                    }
+                    else if (lsbMembership.SelectedIndex == 2)
+                    {
+                        mbf = 25;
+                    }
+                    else if (lsbMembership.SelectedIndex == 3)
+                    {
+                        mbf += 30;
+                    }
+                    else if (lsbMembership.SelectedIndex == -1)
+                    {
+                        lsbMembership.SelectedIndex = 0;
+                        mbf = 40;
+                    }
+                    if (lsbSelectedOptions.Items.Contains("Yoga "))
+                    {
+                        mbf += 10;
+                    }
+                    if (lsbSelectedOptions.Items.Contains("Karate "))
+                    {
+                        mbf += 30;
+                    }
+                    if (lsbSelectedOptions.Items.Contains("Personal Trainer"))
+                    {
+                        mbf += 50;
+                    }
+
+                    total = mbf * month;
+                    lblMonthlyFee.Text = $"${mbf.ToString()}";
+                    lblTotalFee.Text = $"${total.ToString()}";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter integer number", "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Warning);
+            }
+           
+        }
+
+        private void txbMonths_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txbMonths_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char i = e.KeyChar;
+            if (!Char.IsDigit(i) && i != 8 )
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lsbMembership.ClearSelected();
+            lsbSelectedOptions.Items.Clear();
+            lblMonthlyFee.Text = "                          ";
+            lblTotalFee.Text = "                          ";
+            txbMonths.Clear();
+            cbxOptions.Text = "          ";
+        }
+    }
+    }
+
